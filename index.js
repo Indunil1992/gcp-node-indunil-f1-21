500a7f2ad3b6cede0127029a271e754e0b5248f5
Indunil1992 <indunil@adroitlogic.com>
2021-02-01 10:07:37 +0530

    com to GH

---
+++ index.js
@@ -0,0 +1,4 @@
+// This file is used to register all your cloud functions in GCP.
+// DO NOT EDIT/DELETE THIS, UNLESS YOU KNOW WHAT YOU ARE DOING.
+
+exports.gcpNodeIndunilF121 = require("./gcp-node-indunil-f1-21.js").handler;
